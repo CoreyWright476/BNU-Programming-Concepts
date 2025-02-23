0fd30511bd1081ba7717c74ea03c808e44a45a03
CoreyWright476 <corey.wright476@gmail.com>
2025-02-23 12:07:05 +0000

    excluded certain files with the addition of a gitignore, Refactored packages into modules enabling the ability to run each week individually

--- Main.java
+++ Main.java
@@ -3,11 +3,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Week1 week1 = new Week1();
-        Week2 week2 = new Week2();
-
-        week1.runWeek1();
-        week2.runWeek2();
-
     }
 }
--- WeekOne/src/Main.java
+++ WeekOne/src/Main.java
@@ -1,5 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         Week1 week1 = new Week1();
 
         week1.runWeek1();
